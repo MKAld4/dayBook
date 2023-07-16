93afec3ce8ef10d44c21ec69300e4c7a938b834e
Maryia Kachko <alicedall097@gmail.com>
2023-07-16 19:57:17 +0300

    New app 'events' created, main/view.events moved to events/views.py

--- journal/main/views.py
+++ journal/main/views.py
@@ -5,14 +5,13 @@ from main.weather import my_weather, get_weather_json
 # For displaying dictionary
 from django.template.defaulttags import register
 
+
 # Main weather page
 def index(request):
     return render(request, 'index.html', {'my_weather': my_weather})
 
 
 # Substitute for router?
-def events(request):
-    return render(request, 'events.html')
 
 
 # Searching
@@ -28,8 +27,3 @@ def search_weather(request):
             return render(request, 'search_weather.html', {})
     else:
         return render(request, 'search_weather.html', {})
-
-# Searching
-# @register.filter
-# def get_weather_dict(my_weather, key):
-#     return my_weather.get(key)
