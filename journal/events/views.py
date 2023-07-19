f43b9d1c6624b634d8e8e7851654b647af7ce6ef
Maryia Kachko <alicedall097@gmail.com>
2023-07-19 18:58:11 +0300

    Connection between EventForm and model Event.

--- journal/events/views.py
+++ journal/events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.viewsets import ModelViewSet
 
 from events.forms import EventForm
@@ -16,8 +16,25 @@ class EventsView(ModelViewSet):
 
 
 def create(request):
+    error = ''
+    # Get data from form, post - data take from form
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        # If data correct - save it in table
+        if form.is_valid():
+            form.save()
+            # Return on News page if success
+            # Import redirect for this
+            return redirect('home')
+        else:
+            error = 'Wrong form'
+    # Make obj on that bases on the class
     form = EventForm()
+    # Dictionary
     data = {
-        'form': form
+        # Give in create.html by the key
+        'form': form,
+        'error': error
     }
+
     return render(request, 'events_form.html', data)
