b7fa53f92e1a2e1ce2e443928960739f002fbedc
Maryia Kachko <alicedall097@gmail.com>
2023-07-20 18:31:35 +0300

    Url for id.

--- journal/events/urls.py
+++ journal/events/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.events, name='events'),
     path('create/', views.create, name='create'),
-    # path('<int:pk>', )
+    path('<int:pk>/update', views.EventUpdateView.as_view(), name='event-update')
 
 ]
