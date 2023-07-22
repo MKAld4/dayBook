1ec798cf0bec98f313c99e2c71e5010a5655e898
Maryia Kachko <alicedall097@gmail.com>
2023-07-22 20:46:44 +0300

    init commit for entire project's files

---
+++ journal/main/tests/test_my_weather.py
@@ -0,0 +1,10 @@
+# from rest_framework.test import APITestCase
+# from journal.settings import API_KEY
+#
+#
+# # url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+# # BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+# class MyWeatherTestCase(APITestCase):
+#     def test_get(self):
+#         url = f"http://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q=MINSK"
+#         print(url)
